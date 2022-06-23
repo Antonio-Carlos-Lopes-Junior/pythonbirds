9f912ddb740d7881c188147ef54955cdd7e475e0
Antonio-Carlos-Lopes-Junior <antoniocljunior@gmail.com>
2022-06-23 20:49:14 -0300

    Criado testes de motor e direção

---
+++ oo/teste_carro.py
@@ -0,0 +1,41 @@
+from unittest import TestCase
+
+from oo.carro import Motor, Direcao, Carro
+
+
+class CarroTestCase(TestCase):
+    def teste_velocidade_inicial(self):
+        motor = Motor()
+        self.assertEqual(0, motor.velocidade)
+
+    def teste_acelerar(self):
+        motor = Motor()
+        motor.acelerar()
+        self.assertEqual(1, motor.velocidade)
+
+    def teste_frear(self):
+        motor = Motor()
+        motor.acelerar()
+        motor.acelerar()
+        motor.frear()
+        self.assertEqual(0, motor.velocidade)
+        motor.acelerar()
+        motor.acelerar()
+        motor.acelerar()
+        motor.frear()
+        self.assertEqual(1, motor.velocidade)
+
+    def teste_direcao_inicial(self):
+        direcao = Direcao()
+        self.assertEqual('Norte', direcao.valor)
+
+    def teste_girar_a_direita(self):
+        direcao = Direcao()
+        direcao.girar_a_direita()
+        self.assertEqual('Leste', direcao.valor)
+
+    def teste_girar_a_esquerda(self):
+        direcao = Direcao()
+        direcao.girar_a_esquerda()
+        self.assertEqual('Oeste', direcao.valor)
+
