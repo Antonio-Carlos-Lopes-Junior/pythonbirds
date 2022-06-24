cd5441c51106e65da2d2b6f42a29d25f2e931d64
Antonio-Carlos-Lopes-Junior <antoniocljunior@gmail.com>
2022-06-23 21:18:39 -0300

    Feito sobrescrita de atributo

--- oo/teste_carro.py
+++ oo/teste_carro.py
@@ -39,3 +39,39 @@ class CarroTestCase(TestCase):
         direcao.girar_a_esquerda()
         self.assertEqual('Oeste', direcao.valor)
 
+    def teste_carro_direcao_inicial(self):
+        motor = Motor()
+        direcao = Direcao()
+        carro = Carro(direcao, motor)
+        self.assertEqual('Norte', carro.calcular_direcao())
+
+    '''def teste_acelerar(self):
+        motor = Motor()
+        motor.acelerar()
+        self.assertEqual(1, motor.velocidade)
+
+    def teste_frear(self):
+        motor = Motor()
+        motor.acelerar()
+        motor.acelerar()
+        motor.frear()
+        self.assertEqual(0, motor.velocidade)
+        motor.acelerar()
+        motor.acelerar()
+        motor.acelerar()
+        motor.frear()
+        self.assertEqual(1, motor.velocidade)
+
+    def teste_direcao_inicial(self):
+        direcao = Direcao()
+        self.assertEqual('Norte', direcao.valor)
+
+    def teste_girar_a_direita(self):
+        direcao = Direcao()
+        direcao.girar_a_direita()
+        self.assertEqual('Leste', direcao.valor)
+
+    def teste_girar_a_esquerda(self):
+        direcao = Direcao()
+        direcao.girar_a_esquerda()
+        self.assertEqual('Oeste', direcao.valor)'''
