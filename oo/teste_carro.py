87ff520673eec3c0c9dd1ca503b9f506854180f6
Antonio-Carlos-Lopes-Junior <antoniocljunior@gmail.com>
2022-07-04 19:03:35 -0300

    Finalizado e implementado todos os testes em carro

--- oo/teste_carro.py
+++ oo/teste_carro.py
@@ -39,39 +39,55 @@ class CarroTestCase(TestCase):
         direcao.girar_a_esquerda()
         self.assertEqual('Oeste', direcao.valor)
 
-    def teste_carro_direcao_inicial(self):
-        motor = Motor()
+    def teste_velocidade_inicial_carro(self):
         direcao = Direcao()
+        motor = Motor()
+        carro = Carro(direcao, motor)
+        self.assertEqual(0, carro.calcular_velocidade())
+
+    def teste_acelerar_carro(self):
+        direcao = Direcao()
+        motor = Motor()
+        carro = Carro(direcao, motor)
+        carro.acelerar()
+        self.assertEqual(1, carro.calcular_velocidade())
+
+    def teste_frear_carro(self):
+        direcao = Direcao()
+        motor = Motor()
+        carro = Carro(direcao, motor)
+        carro.acelerar()
+        carro.acelerar()
+        carro.frear()
+        self.assertEqual(0, carro.calcular_velocidade())
+        carro.acelerar()
+        carro.acelerar()
+        carro.acelerar()
+        carro.frear()
+        self.assertEqual(1, carro.calcular_velocidade())
+
+    def teste_direcao_inicial_carro(self):
+        direcao = Direcao()
+        motor = Motor()
         carro = Carro(direcao, motor)
         self.assertEqual('Norte', carro.calcular_direcao())
 
-    '''def teste_acelerar(self):
-        motor = Motor()
-        motor.acelerar()
-        self.assertEqual(1, motor.velocidade)
-
-    def teste_frear(self):
-        motor = Motor()
-        motor.acelerar()
-        motor.acelerar()
-        motor.frear()
-        self.assertEqual(0, motor.velocidade)
-        motor.acelerar()
-        motor.acelerar()
-        motor.acelerar()
-        motor.frear()
-        self.assertEqual(1, motor.velocidade)
-
-    def teste_direcao_inicial(self):
+    def teste_direcao_inicial_carro(self):
         direcao = Direcao()
-        self.assertEqual('Norte', direcao.valor)
+        motor = Motor()
+        carro = Carro(direcao, motor)
+        self.assertEqual('Norte', carro.calcular_direcao())
 
-    def teste_girar_a_direita(self):
+    def teste_girar_a_direita_carro(self):
         direcao = Direcao()
+        motor = Motor()
+        carro = Carro(direcao, motor)
         direcao.girar_a_direita()
-        self.assertEqual('Leste', direcao.valor)
+        self.assertEqual('Leste', carro.calcular_direcao())
 
-    def teste_girar_a_esquerda(self):
+    def teste_girar_a_esquerda_carro(self):
         direcao = Direcao()
+        motor = Motor()
+        carro = Carro(direcao, motor)
         direcao.girar_a_esquerda()
-        self.assertEqual('Oeste', direcao.valor)'''
+        self.assertEqual('Oeste', carro.calcular_direcao())
